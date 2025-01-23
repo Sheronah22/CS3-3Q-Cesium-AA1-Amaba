aa49e919d9d751a0356482909c16ae6eb73d2f5d
sfaamaba-src <sfaamaba22@src.pshs.edu.ph>
2025-01-23 09:01:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let pi = 3.14;
+let r = 6.378e6;
+let c = 2*pi*r;
+let a = 4*pi*r^2;
+let v = 4/3*pi*r^3;
+
+document.write('The radius of the sphere is ' + r + '<br>');
+document.write('The circumference is ' + c + '<br>');
+document.write('The surface area is ' + a + '<br>');
+document.write('The volume is ' + v + '<br>');
